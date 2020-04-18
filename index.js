5d41a06f9601b0b7540dcd67a4c4513aefacb00d
Arpit Chandak <arpitchandak123@gmail.com>
2020-04-18 22:45:45 +0530

    deploy

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const favicon = require('express-favicon');
+
 const app = express()
 const PORT = process.env.PORT || 80
 
 const {mongourl} = require('./keys')
 require('./User');
+
+app.use(favicon(__dirname + '/favicon.ico'));
 
 const authroutes = require('./authroutes');
 app.use(bodyParser.json())
