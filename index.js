1a1ef18eec33f3ee72ca89bdddbda3d26f0d50b0
Arpit Chandak <arpitchandak123@gmail.com>
2020-04-18 19:51:33 +0530

    testing

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const app = express()
+const PORT = 3000
+
+const {mongourl} = require('./keys')
+require('./modules/User');
+
+const authroutes = require('./routes/authroutes');
+app.use(bodyParser.json())
+app.use(authroutes)
+
+
+
+mongoose.connect(mongourl,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+mongoose.connection.on('connected', () =>{
+    console.log("Connected to mongo")
+})
+
+mongoose.connection.on('error', (error) =>{
+    console.log("Not Connected to mongo", error)
+})
+
+
+app.post('/',(request,response) =>{
+    console.log(request.body)
+    response.send("Hello");
+})
+
+app.listen(PORT, () => {
+    console.log("Server Running on :", PORT)
+})
+
---
+++ routes/authroutes.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = express.Router() 
+const mongoose = require('mongoose')
+const User = mongoose.model('User')
+
+router.post('/data',async (request,response) =>{
+
+
+
+    const {first_name,last_name,email,avatar} = request.body
+    
+    try {
+        const user = new User({first_name,last_name,email,avatar})
+        await user.save()   
+        response.send("Data Recived")
+        console.log(request.body)
+
+ 
+    } catch (err) {
+        response.status(404).send(err.message)
+        console.log(err.message)
+
+    }
+   
+} )
+
+module.exports = router
