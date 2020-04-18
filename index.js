3a26acddc392f4dceaae3bd1f8ccd57b5ead40ec
Arpit Chandak <arpitchandak123@gmail.com>
2020-04-18 20:42:29 +0530

    Deploying

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const app = express()
-const PORT = 3000
+const PORT = 80
 
 const {mongourl} = require('./keys')
 require('./modules/User');
